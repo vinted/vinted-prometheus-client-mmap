9d2a5ae0eb5660d6c0c4e58d68b7860a17c33a09
Giedrius Statkevičius <giedriuswork@gmail.com>
2024-03-25 12:05:03 +0200

    ext/fast_mmaped_file_rs: add protobuf compilation step (#2)
    Breaking out protobuf compilation step from
    https://gitlab.com/gitlab-org/ruby/gems/prometheus-client-mmap/-/merge_requests/132.
    
    Tested by compiling and checking that
    `tmp/x86_64-linux/fast_mmaped_file_rs/3.1.2/target/release/build/fast_mmaped_file_rs-f298ac7ef311d558/out/io.prometheus.client.rs`
    exists.
    
    Signed-off-by: Giedrius Statkevičius <giedrius.statkevicius@vinted.com>

--- ext/fast_mmaped_file_rs/build.rs
+++ ext/fast_mmaped_file_rs/build.rs
@@ -1,5 +1,7 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let _ = rb_sys_env::activate()?;
+    prost_build::compile_protos(&["src/metrics.proto"], &["src/"])
+        .expect("failed compile protobufs");
 
     Ok(())
 }
